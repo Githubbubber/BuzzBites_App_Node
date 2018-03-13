cc74686b40af088646f011d773e726e4195056fe
githubbubber <mekesiabrown@gmail.com>
2018-03-13 06:38:00 -0400

    Tailored port to Heroku

--- app.js
+++ app.js
@@ -130,4 +130,8 @@ app.post('/', function(req, res){
 
 // Everything else 
 app.get('/*', function(req, res) { res.status(404).render('error'); });
-if (!module.parent) { app.listen(8080); console.log('EJS Demo server started on port 8080'); }
+if (!module.parent) {
+	app.listen(process.env.PORT || 8080, function(){
+  		console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+	});
+}
